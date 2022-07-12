ff5bfdf2eea7a311269e85e828db818fb97a2b54
Fernando Corrêa <paracorrea@gmail.com>
2022-07-12 09:18:11 -0300

    Projeto Parse iniciado

---
+++ src/javaDomParser/MyDomParser.java
@@ -0,0 +1,33 @@
+package javaDomParser;
+
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class MyDomParser {
+
+	public static void main(String[] args) {
+
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		try {
+			DocumentBuilder builder =  factory.newDocumentBuilder();
+			Document doc = builder.parse("people.xml");
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
