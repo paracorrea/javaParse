f78a7d120e2f92c0b0951908116f930234292756
Fernando Corrêa <paracorrea@gmail.com>
2022-07-12 10:26:37 -0300

    Finalizado leitura do XML

--- src/javaDomParser/MyDomParser.java
+++ src/javaDomParser/MyDomParser.java
@@ -7,16 +7,39 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class MyDomParser {
 
 	public static void main(String[] args) {
-
+		
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		try {
 			DocumentBuilder builder =  factory.newDocumentBuilder();
 			Document doc = builder.parse("people.xml");
+			NodeList personList =  doc.getElementsByTagName("people");
+			
+			for (int i=0; i<personList.getLength(); i++) {
+				Node p = personList.item(i);
+				if(p.getNodeType()==Node.ELEMENT_NODE) {
+					Element person =(Element) p;
+					String id = person.getAttribute("id");
+					NodeList nameList = person.getChildNodes();
+					for (int j=1; j<nameList.getLength();j++) {
+						Node n = nameList.item(j);
+						if(n.getNodeType()==Node.ELEMENT_NODE) {
+							Element name = (Element) n;
+							System.out.println("Person " + id + ":"+ name.getTagName()
+							+"="+name.getTextContent());
+						}
+					
+					
+					}
+			}
+			}
 		} catch (ParserConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
